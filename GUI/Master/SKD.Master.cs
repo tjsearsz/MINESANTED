58611935563dac309a986447ef530cef52449da5
svillami <sandravillamizar87@gmail.com>
2015-10-22 14:50:46 -0430

    M16_SubiendoVerdadero
    Ultima actualizacion de Sakaratedo

---
+++ GUI/Master/SKD.Master.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Text;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace templateApp
+{
+    public partial class SKD : System.Web.UI.MasterPage
+    {
+        private string idModulo;
+        private Dictionary<string, string> opcionesDelMenu = new Dictionary<string, string>();
+
+        public string IdModulo
+        {
+            get { return idModulo; }
+            set { idModulo = value; }
+        }
+        public Dictionary<string, string> OpcionesDelMenu
+        {
+            get { return opcionesDelMenu; }
+            set { opcionesDelMenu = value; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Server.MapPath("~/GUI/Master/menuLateral.xml"));
+            idModulo = IdModulo;
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+                foreach (XmlNode subNode in node.ChildNodes)
+                    if (!(subNode.Attributes["id"] == null) && subNode.Attributes["id"].InnerText.Equals(IdModulo))
+                    {
+                        OpcionesDelMenu[node.Attributes["nombre"].InnerText] = node.Attributes["link"].InnerText;
+                        break;
+                    }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MINESANTED
+# SA-KARATEDO
+Sistema informático vía web que apoya el seguimiento y control de la gestión de los procesos administrativos cotidianos llevados a cabo por los entrenadores y administradores de Organizaciones y/o Dojos del deporte de Karate-Do.
