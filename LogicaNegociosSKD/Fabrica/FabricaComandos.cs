a2e87d04e32600c5a15fd3a510a614cd3940b78b
Minervavr <minerva_veru@hotmail.com>
2015-12-19 17:20:49 -0430

    M16_Fabricacomandos
    se incorpora la fabrica en la capa de logica

--- LogicaNegociosSKD/Fabrica/FabricaComandos.cs
+++ LogicaNegociosSKD/Fabrica/FabricaComandos.cs
@@ -2,11 +2,74 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using DominioSKD;
+using ComandosSKD.Modulo16;
 
-namespace LogicaNegociosSKD.Fabrica
+namespace ComandosSKD.Fabrica
 {
-    class FabricaComandos
+    public class FabricaComandos
     {
+        #region Modulo 16
+        public static Comando<Entidad, bool> crearComandoagregarEventoaCarrito()
+        {
+            return new ComanadoagregarEventoaCarrito();
+        }
+
+        public static Comando<Entidad, bool> CrearComandoagregarImplementoaCarrito()
+        {
+            return new ComandoagregarImplementoaCarrito();
+        }
+
+        public static Comando<Entidad, bool> CrearComandoagregarMatriculaaCarrito()
+        {
+            return new ComandoagregarMatriculaaCarrito();
+        }
+
+        public static Comando<Entidad, bool> CrearComandoeliminarItem()
+        {
+            return new ComandoeliminarItem();
+        }
+        public static Comando<Entidad, Entidad> CrearComandogetEvento()
+        {
+            return new ComandogetEvento();
+        }
+
+        public static Comando<Entidad, Entidad> CrearComandoGetImplemento()
+        {
+            return new ComandoGetImplemento();
+        }
+
+        public static Comando<Entidad, Entidad> CrearComandoGetMatricula()
+        {
+            return new ComandoGetMatricula();
+        }
+
+        public static Comando<Entidad, bool> CrearComandomodificarCarritoevento()
+        {
+            return new ComandomodificarCarritoevento();
+        }
+
+        public static Comando<Entidad, bool> CrearComandomodificarCarritoimplemento()
+        {
+            return new ComandomodificarCarritoimplemento();
+        }
+
+
+        public static Comando<Entidad, bool> CrearComandomodificarCarritomatricula()
+        {
+            return new ComandomodificarCarritomatricula();
+        }
+
+
+        public static Comando<Entidad, bool> CrearComandoregistrarpago()
+        {
+            return new Comandoregistrarpago();
+        }
+
+
+
+        #endregion
+
+
     }
 }
