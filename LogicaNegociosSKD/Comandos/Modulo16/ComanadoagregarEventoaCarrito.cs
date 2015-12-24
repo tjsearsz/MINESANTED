b4e765188a3263da27a7e04689079091c88f6c90
svillami <sandravillamizar87@gmail.com>
2015-12-24 11:02:07 -0430

    Subiendo archivos de teddy
    Archivos de teddy segun la direccion que salia en la consola del repo.

---
+++ LogicaNegociosSKD/Comandos/Modulo16/ComanadoagregarEventoaCarrito.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominioSKD;
+using DominioSKD.Entidades.Modulo16;
+using DatosSKD.InterfazDAO.Modulo16;
+using DatosSKD.FabricaDAO;
+using ComandosSKD;
+using DominioSKD;
+using DatosSKD.Modulo16;
+using ExcepcionesSKD.Modulo16.ExcepcionesDeDatos;
+using ExcepcionesSKD.Modulo16;
+using ExcepcionesSKD;
+using DatosSKD.DAO.Modulo16;
+
+
+namespace ComandosSKD.Modulo16
+{
+    class ComanadoagregarEventoaCarrito : Comando<Entidad>
+    {
+
+        /// <summary>
+        /// Comando que ejecuta la logica para agregar evento al carrito
+        /// </summary>
+        /// <param name="parametro">Entidad de tipo Evento</param>
+        /// <returns>retorna true si se inserto el evento satisfactoriamente,
+        /// de lo contrario devueleve false</returns>
+        public override bool Ejecutar(Entidad parametro)
+        {
+
+            try
+            {
+
+                FabricaDAOSqlServer laFabrica = new FabricaDAOSqlServer();
+                IdaoCarrito daoCarrito = laFabrica.ObtenerDAOCarrito();
+                //Retonarmos el exito o fallo del proceso
+                return daoCarrito.Agregar(parametro);
+
+                //Escribo en el logger la entrada a este metodo
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                    RecursosLogicaModulo16.MENSAJE_ENTRADA_LOGGER,
+                    System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+
+                //Escribo en el logger la salida a este metodo
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                    RecursosLogicaModulo16.MENSAJE_SALIDA_LOGGER, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            }
+            #region Catches
+            catch (LoggerException e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw e;
+            }
+            catch (ParseoVacioException e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw e;
+            }
+            catch (ParseoFormatoInvalidoException e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw e;
+            }
+            catch (ParseoEnSobrecargaException e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw e;
+            }
+            catch (ParametroInvalidoException e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw e;
+            }
+            catch (ExceptionSKDConexionBD e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw e;
+            }
+            catch (ExceptionSKD e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw e;
+            }
+            catch (Exception e)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, e);
+                throw new ExceptionSKDConexionBD(RecursosLogicaModulo16.CODIGO_EXCEPCION_GENERICO,
+                    RecursosLogicaModulo16.MENSAJE_EXCEPCION_GENERICO, e);
+            }
+
+            #endregion
+
+
+
+
+
+        }
+
+        
+    }
+}
