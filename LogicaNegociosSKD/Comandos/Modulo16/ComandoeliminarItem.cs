febe50217a8c5215e03e52ecf86c48fc9857dee3
Minervavr <minerva_veru@hotmail.com>
2015-12-27 17:06:00 -0430

    arreglo dedel merge en el comando eliminaritem
    se arregla el conflicto ocurrido en el comando eliminar item

--- LogicaNegociosSKD/Comandos/Modulo16/ComandoeliminarItem.cs
+++ LogicaNegociosSKD/Comandos/Modulo16/ComandoeliminarItem.cs
@@ -1,50 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DominioSKD;
-<<<<<<< HEAD
-using DatosSKD.FabricaDAO;
+﻿using DominioSKD.Entidades.Modulo16;
 using DatosSKD.InterfazDAO.Modulo16;
-using DatosSKD.InterfazDAO;
-using ExcepcionesSKD;
-using ExcepcionesSKD.Modulo16;
-using DominioSKD.Entidades.Modulo16;
+using DatosSKD.FabricaDAO;
+using LogicaNegociosSKD.Comandos;
+using DominioSKD;
 using DominioSKD.Fabrica;
-
-namespace LogicaNegociosSKD.Comandos.Modulo16
-{
-    class ComandoeliminarItem : Comando<bool>
-    {
-      //  private Persona p= new Persona();
-=======
-using DominioSKD.Entidades.Modulo16;
-using DatosSKD.InterfazDAO.Modulo16;
-using DatosSKD.FabricaDAO;
-using ComandosSKD;
-using DominioSKD;
-using DatosSKD.Modulo16;
 using ExcepcionesSKD.Modulo16.ExcepcionesDeDatos;
 using ExcepcionesSKD.Modulo16;
 using ExcepcionesSKD;
 using DatosSKD.DAO.Modulo16;
 using ComandosSKD.Modulo16;
 
+ 
+
 namespace ComandosSKD.Modulo16
 {
-    class ComandoeliminarItem : Comando<Entidad>
+    class ComandoeliminarItem : Comando<bool>
     {
 
 
-        #region Atributos
-        int _tipobj;
-        int _objbor;
-        bool _e;
-        Entidad _param;
-        #endregion
-
->>>>>>> b4e765188a3263da27a7e04689079091c88f6c90
+        
 
         /// <summary>
         /// Comando que ejecuta la logica para eliminar un item del carrito
@@ -59,19 +33,17 @@ namespace ComandosSKD.Modulo16
             {
                 FabricaDAOSqlServer laFabrica = new FabricaDAOSqlServer();
                 IdaoCarrito daoCarrito = laFabrica.ObtenerDAOCarrito();
-<<<<<<< HEAD
+ 
                 Persona  p = FabricaEntidades.ObtenerPersona() as Persona;
             
                 p.Id =  7;
                  
                 //Retornamos la respuesta  
                 return daoCarrito.eliminarItem(7, 3, p );
-=======
+ 
 
 
-                //Retornamos la respuesta  
-                return daoCarrito.eliminarItem(1, 1, _param);
->>>>>>> b4e765188a3263da27a7e04689079091c88f6c90
+               
 
 
             }
@@ -127,15 +99,7 @@ namespace ComandosSKD.Modulo16
 
         }
 
-<<<<<<< HEAD
-       
-=======
-        public ComandoeliminarItem(bool e)
-        {
-            _e = e;
-
-        }
->>>>>>> b4e765188a3263da27a7e04689079091c88f6c90
+ 
 
     }
 }
